a186f4abd46472d0ec0fee3c60a237dfcf1cb6eb
gflores93 <gflores.93@hotmail.com>
2022-09-06 13:27:44 +0200

    Authentication module created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AuthGuard } from './auth/services/auth-guard.service';
 import { CartComponent } from './components/cart/cart.component';
 import { ProductsComponent } from './components/products/products.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'products', pathMatch: 'full' },
-  { path: 'products', component: ProductsComponent },
-  { path: 'cart', component: CartComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'products', canActivate: [AuthGuard], component: ProductsComponent },
+  { path: 'cart', canActivate: [AuthGuard], component: CartComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
